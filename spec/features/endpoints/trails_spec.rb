6560309900e0ed166418ad246cc90febe9622b5c
Jesse Gietzen <gietzenjesse@gmail.com>
2020-08-03 11:09:49 -0600

    add to test

--- spec/features/endpoints/trails_spec.rb
+++ spec/features/endpoints/trails_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe 'trails endpoint', type: :request do
     get '/api/v1/trails?location=denver,co'
 
     expect(response).to be_successful
-    json = JSON.parse(response.body)
+    json = JSON.parse(response.body, symbolize_names: true)
+
     expect(json[:data][:attributes].size).to eq(3)
+    expect(json[:data][:attributes][:location][:city]).to eq('Denver')
+    expect(json[:data][:attributes][:forecast][:temp].class).to eq(Float)
+    expect(json[:data][:attributes][:trails][0][:name]).to_not be_nil
   end
 end

1e3c9fa811470981c507c4a7d9e761b1ad42d783
Jesse Gietzen <gietzenjesse@gmail.com>
2020-08-03 09:46:41 -0600

    add trails list poro model

---
+++ app/poro/trails_list.rb
@@ -0,0 +1,11 @@
+class TrailsList
+  attr_reader :id, :location, :forecast, :trails
+
+  def initialize(city_state)
+    @id = nil
+    @location = LocationService.new.get_coordinates(city_state)
+    weather = OpenWeatherService.new.get_forecast(@location[:coordinates])
+    @forecast = @weather[:current]
+    @trails = TrailsService.new.get_trails(@location[:coordinates])
+  end
+end

4717ee724e77be815700e498abf2f77dd78584eb
Jesse Gietzen <gietzenjesse@gmail.com>
2020-08-03 11:11:35 -0600

    add origin coordinates to trail poro model

--- app/poro/trail.rb
+++ app/poro/trail.rb
@@ -1,12 +1,11 @@
 class Trail
   attr_reader :name, :summary, :difficulty, :location, :distance_to_trail
 
-  def initialize(trail_info)
-    binding.pry
+  def initialize(trail_info, origin)
     @name = trail_info[:name]
     @summary = trail_info[:summary]
     @difficulty = trail_info[:difficulty]
     @location = trail_info[:location]
-    @distance_to_trail = # GET DISTANCE BLAAARGH
+    @distance_to_trail = DirectionsService.new.trail_distance(trail_info, origin)
   end
 end

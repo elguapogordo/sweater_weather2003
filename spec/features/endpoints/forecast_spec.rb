0a4f7c3afec8bc83626e29d758d174cc518104d1
Jesse Gietzen <gietzenjesse@gmail.com>
2020-08-03 09:23:52 -0600

    add to testing

--- spec/features/endpoints/forecast_spec.rb
+++ spec/features/endpoints/forecast_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'forecast endpoint' do
-  xit 'returns forecast info for a location' do
-    visit '/api/v1/forecast?location=denver,co'
+RSpec.describe 'forecast endpoint', type: :request do
+  it 'returns forecast info for a location' do
+    get '/api/v1/forecast?location=denver,co'
 
     expect(response).to be_successful
+    json = JSON.parse(response.body)
+    expect(json[:data][:attributes].size).to eq(4)
   end
 end

0e835f163711074f364b151fe9fe2bb72eebe39d
Jesse Gietzen <gietzenjesse@gmail.com>
2020-08-03 09:25:15 -0600

    add initial test

---
+++ spec/features/endpoints/trails_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'trails endpoint', type: :request do
+  it 'returns trails info for a location' do
+    get '/api/v1/trails?location=denver,co'
+
+    expect(response).to be_successful
+    json = JSON.parse(response.body)
+    expect(json[:data][:attributes]).to_not be_empty
+  end
+end

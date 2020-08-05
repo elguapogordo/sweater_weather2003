0833f2fab6ef2c3369768c997e66d96793a24e1a
Jesse Gietzen <gietzenjesse@gmail.com>
2020-08-04 21:11:38 -0600

    add sessions controller with create action

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user = User.find_by_email(params[:email])
+    unless user.nil?
+      if user.authenticate(params[:password])
+        render json: UsersSerializer.new(user)
+      else
+        render json: { error: 'Password authentication failed' }, status: 400
+      end
+    else
+      render json: { error: 'User not found'}, status: 400
+    end
+  end
+end

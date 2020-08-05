8b63bcda191b116c42b6e36f6ca4fad9d6393a44
Jesse Gietzen <gietzenjesse@gmail.com>
2020-08-04 19:21:23 -0600

    update test, sad paths added

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -21,7 +21,63 @@ RSpec.describe 'users endpoint', type: :request do
     expect(user[:data][:attributes][:api_key]).not_to be_nil
   end
 
-  xit 'validates email uniqueness' do
-    User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password')
+  it 'validates email presence' do
+
+    post '/api/v1/users', params: {
+      password: 'password',
+      password_confirmation: 'password'
+    }
+
+    expect(response.status).to eq(400)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:error][0]).to include('Email can\'t be blank')
   end
+
+  it 'validates email uniqueness' do
+
+    User.create!(email: 'whatever@example.com', password: 'password', password_confirmation: 'password')
+
+    post '/api/v1/users', params: {
+      email: 'whatever@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    }
+
+    expect(response.status).to eq(400)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:error][0]).to include('Email has already been taken')
+  end
+
+  it 'validates password presence' do
+
+    post '/api/v1/users', params: {
+      email: 'whatever@example.com'
+    }
+
+    expect(response.status).to eq(400)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:error][0]).to include('Password can\'t be blank')
+  end
+
+  it 'validates password confirmation' do
+
+    post '/api/v1/users', params: {
+      email: 'whatever@example.com',
+      password: 'password',
+      password_confirmation: 'tomato bisque'
+    }
+
+    expect(response.status).to eq(400)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json[:error][0]).to include('Password confirmation doesn\'t match Password')
+  end
+
 end
